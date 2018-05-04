a429e2d534bde41091515b271b10b5a707fbdc06
wangzhe.bj <wangzhe.bj@fang.com>
2018-05-04 15:54:28 +0800

    zuul

--- feign/src/main/java/net/megatree/feign/SchedualServiceHi.java
+++ feign/src/main/java/net/megatree/feign/SchedualServiceHi.java
@@ -1,6 +1,7 @@
 package net.megatree.feign;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -8,9 +9,19 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by wangzhe.bj on 2018/5/3.
  */
 
-@FeignClient("service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHi.SchedualServiceHiHystrix.class)
 public interface SchedualServiceHi {
 
     @GetMapping("/hi")
     String sayHiFromClientOne(@RequestParam(value = "name")String name);
+
+
+    @Component
+    class SchedualServiceHiHystrix implements SchedualServiceHi{
+
+        @Override
+        public String sayHiFromClientOne(String name) {
+            return "feign: sorry, "+name;
+        }
+    }
 }
--- service-ribbon/src/main/java/net/megatree/serviceribbon/HelloController.java
+++ service-ribbon/src/main/java/net/megatree/serviceribbon/HelloController.java
@@ -17,4 +17,5 @@ public class HelloController {
     public String hi(String name) {
         return helloService.hiService(name);
     }
+
 }
