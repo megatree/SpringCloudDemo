40a6341f17e606c00bdcae82340a3a3d01584ee2
wz.myth <mythss@foxmail.com>
2018-09-15 00:31:39 +0800

    例行更新

---
+++ service-hi/src/main/java/net/megatree/servicehi/LeakBean.java
@@ -0,0 +1,10 @@
+package net.megatree.servicehi;
+
+public class LeakBean {
+
+    public String str;
+
+    public LeakBean(String str) {
+        this.str = str;
+    }
+}
--- service-hi/src/main/java/net/megatree/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/net/megatree/servicehi/ServiceHiApplication.java
@@ -8,10 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class ServiceHiApplication {
+
+    public static List<LeakBean> list = new ArrayList<>();
+    static Object lock = new Object();
+    static Object lock2 = new Object();
+
 
     @Value("${server.port}")
     String port;
@@ -21,7 +30,47 @@ public class ServiceHiApplication {
     }
 
     @RequestMapping("/hi")
-    public String home(String name) {
+    public String home(String name) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(10);
+
         return "hi " + name + ",i am from port:" + port;
+    }
+
+    @RequestMapping("/heap")
+    public Object heap(){
+        for (int i = 0; i < 10000; i++) {
+            list.add(new LeakBean(new StringBuffer(i).append("abcd").toString()));
+
+        }
+        return list;
+    }
+
+    @RequestMapping("dead1")
+    public Object deadLock() throws InterruptedException {
+
+
+        synchronized (lock){
+            TimeUnit.SECONDS.sleep(10);
+
+            synchronized (lock2){
+                System.out.println("拿到了两把锁");
+            }
+        }
+
+        return "成功返回";
+    }
+
+    @RequestMapping("dead2")
+    public Object deadLock2() throws InterruptedException {
+
+        synchronized (lock2){
+            TimeUnit.SECONDS.sleep(1);
+
+            synchronized (lock){
+                System.out.println("哈哈");
+            }
+        }
+
+        return "成功返回";
     }
 }
